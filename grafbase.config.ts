c6b129aacf60f2ed9892609e4610a9f1faf930de
ShrabanaG <shrabanagoswami@gmail.com>
2024-01-25 19:05:39 +0530

    feat: grafbase.config.ts is added

---
+++ grafbase.config.ts
@@ -0,0 +1,42 @@
+import { graph, config } from "@grafbase/sdk";
+
+// Welcome to Grafbase!
+//
+// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+
+const g = graph.Standalone();
+
+const User = g.type("User", {
+  name: g.string(),
+  email: g.string(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
+  linkedinUrl: g.url().optional(),
+  projects: g
+    .relation(() => Project)
+    .list()
+    .optional(),
+});
+export default config({
+  graph: g,
+  // Authentication - https://grafbase.com/docs/auth
+  auth: {
+    // OpenID Connect
+    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
+    // providers: [oidc],
+    rules: (rules) => {
+      rules.public();
+    },
+  },
+  // Caching - https://grafbase.com/docs/graphql-edge-caching
+  // cache: {
+  //   rules: [
+  //     {
+  //       types: ['Query'], // Cache everything for 60 seconds
+  //       maxAge: 60,
+  //       staleWhileRevalidate: 60
+  //     }
+  //   ]
+  // }
+});
